420be0ec5125bcbd638649ffc8594026bc935692
somename <someemail>
2015-10-21 16:02:10 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -154,4 +154,14 @@ public class PlanetExplorer {
 		}
 		return ans;
 	}
+	
+	public static void main(String[] args)
+	{
+		PlanetExplorer pe = new PlanetExplorer(3, 3, "(2,2)");
+		int i=0,j=0;
+		for (i =0;i<3;i++)
+			for(j=0;j<3;j++){
+				System.out.println(pe.planet[i][j]);
+				System.out.println();}
+	}
 }
