8f42caafc56911d75c479369b9a71d18288a987d
somename <someemail>
2015-10-21 14:49:33 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -4,12 +4,14 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import org.unioulu.tol.sqat2015.planetExplorer.PlanetExplorer;
+
 public class TestPlanetExplorer {
 
 	@Test
 	public void testPlanet() {
-		PlanetExplorer pe = new PlanetExplorer();
-		
+		PlanetExplorer pe = new PlanetExplorer(1, 1, "");
+		assertEquals(1, pe.x);
 	}
 	
 	
